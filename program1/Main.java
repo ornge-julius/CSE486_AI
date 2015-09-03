7dbe09033749c854b91cf0b541d6e1ab6e91c55e
ornge-julius <jcware8@gmail.com>
2015-09-03 19:37:38 -0400

    Finished Main class, Program now spits out robot's performance score

--- program1/Main.java
+++ program1/Main.java
@@ -1,38 +1,56 @@
 /*
 	Simple A.I. program that simulates a robot's performace
-	vacuuming a floor or 4 inline squares.
+	vacuuming a floor of 4 inline squares.
 
 */
 import java.util.Scanner;
+import java.util.Random;
 import java.io.*;
 
 public class Main{
 
 	public static void main(String[] args){
+		double S = Double.parseDouble(args[0]) * 1000;
+		double D = Double.parseDouble(args[1]) * 1000;
 		Robot myRobo = new Robot();
 		int floor[] = new int[4];
 		int trials = 0;
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter number of trials: ");
-		trials = sc.nextInt();
+		int Sx = (int)S;
+		int Dx = (int)D;
+		trials = 10000;
 		
 		for(int i = 0; i < trials; i++){
 			//randomly dirty squares
-			for(int j = 0; j < 4; j++){
-				floor[j] = (int)(Math.random() * 2);
+			//need to randomly generate dirt based on D
+			for(int j = 0; j < floor.length; j++){
+				int result = (int)(Math.random()*100);
+				if(result < Dx){
+					//tile is dirty D % of the time
+					floor[j] = 1;
+				} else {
+					floor[j] = 0;
+				}
 			}
 			
 			//robot clean or move
-			if (floor[myRobo.getPos()] == 1)
-				floor = myRobo.clean(floor);
-			else 
-				myRobo.move();
-			for(int k: floor){
-				if (i == 0)
-				myRobo.tally();
+				
+				if (floor[myRobo.getPos()] == 1){
+					//clean should take an argument S for a suck percentage
+					int result2 = (int)(Math.random()*100);
+					if(result2 < Sx)
+						//clean works S% of the time
+						floor = myRobo.clean(floor);
+				} else { 
+					myRobo.move();
+				for(int k = 0; k < floor.length; k++){
+					// for every clean tile, add to the 
+					// robot's score
+					if (floor[k] == 0)
+					myRobo.tally();
+				}
 			}
 		}
+
+	System.out.println("Robot Score: " + myRobo.getScore());
 	}
 }
-
