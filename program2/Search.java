70339f1bac5682167f5d3683be232796573057a6
ornge-julius <jcware8@gmail.com>
2015-09-11 23:12:59 -0400

    Got parsing functionality to compile and run

--- program2/Search.java
+++ program2/Search.java
@@ -10,48 +10,42 @@ public class Search{
 		String type = args[0];
 		String fileName = args[1];
 		//BufferedReader br = new BufferedReader(new FileReader(fileName));
-		Scanner in = new Scanner(fileName);
-		int row = in.nextInt();
-		int col = in.nextInt();
-		Node [][] board = new Node[row][col];
-		ArrayList<Integer> costList = new ArrayList<Integer>();
+		try{
+			File myFile = new File(fileName);
+			Scanner in = new Scanner(myFile);
+			int row = in.nextInt();
+			int col = in.nextInt();
+			//System.out.println(row + " " +col);
+			Node [][] board = new Node[row][col];
+			ArrayList<Integer> costList = new ArrayList<Integer>();
 
-		//initialze board with empty nodes
-		for(int i = row; i > 0; i--){
-			for(int j = col; j > 0; j--){
-				board[i][j] = new Node(i, j);
-			}
-		}
-		
-		
-		//set messages
-		for(int i = row; i > 0; i--){
-			for(int j = col; j > 0; j--){
-				if(in.next().equals("X")){
-					board[i][j].setMessage();
+				//initialze board with empty nodes
+				for(int i = 0; i < row; i++){
+					for(int j = 0; j < col; j++){
+						board[i][j] = new Node(i, j);
+					}
 				}
+				
+				//set messages
+				for(int i = row; i > 0; i--){
+					for(int j = col; j > 0; j--){
+						if(in.next().equals("X")){
+							board[i-1][j-1].setMessage();
+						}
+					}
+				}
+
+				//set costs
+				for(int i = row; i > 0; i--){
+					for(int j = col; j > 0; j--){
+						board[i-1][j-1].setCost(in.nextDouble());		
+					}
+				}
+
+
+			} catch (Exception ex){
+			ex.printStackTrace();
+			
 			}
-		}
-
-		//read costs
-		String[][] strCosts = new String[row][col];
-		int y = row;
-		String strline ="";
-		while(in.hasNextLine()){
-			strline = in.nextLine();
-			strCosts[y] = strline.split(" ");
-			y--;
-		
-		}
-
-		
-		//parse and set costs
-		for(int i = row; i > 0; i--){
-			for(int j = col; j > 0; j--){
-				board[i][j].setCost(Double.parseDouble(strCosts[i][j]));		
-			}
-		}
-
-
 	}
 }
